828a35324b5c896c38f1129f83882a1a7f794274
Isabellanamutenda <namtendaisabella.com>
2022-01-07 14:12:50 +0300

    required changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  load_and_authorize_resource
   def index
-    @user = User.includes(:posts).find(params[:user_id])
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.includes(:comments, :likes).find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -15,25 +14,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    if current_user
-      @post = current_user.posts.new(params[:id])
-      @post.title = params[:title]
-      @post.text = params[:text]
-      if @post.save
-        flash[:notice] = 'Post successfully added!'
-        redirect_to user_posts_path
-      else
-        render 'new'
-      end
+    @post = Post.new(params[:id])
+    @post.title = params[:title]
+    @post.text = params[:text]
+    @post.author_id = current_user.id
+    if @post.save
+      flash[:notice] = 'Post successfully added!'
+      redirect_to user_posts_path
     else
-      redirect_to new_user_session_path, notice: 'You are not logged in.'
+      render 'new'
     end
-  end
-
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    flash[:notice] = 'Post Deleted Succefully'
-    redirect_back fallback_location: root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
   def index
-    if current_user
-      @users = User.all
-    else
-      redirect_to user_session_path
-    end
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.recent_posts
   end
 end
