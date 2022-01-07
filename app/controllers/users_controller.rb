a1f6cb31c83ac64b5e0dd167b2689dc90c40148a
Isabellanamutenda <namtendaisabella.com>
2022-01-07 15:37:49 +0300

    fix rails errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user.users.find(params[:id])
     @posts = @user.recent_posts
   end
 end
