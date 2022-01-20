9b353bc1e566c876807cc9638d725e61cf479fba
Isabellanamutenda <namtendaisabella.com>
2022-01-20 12:30:35 +0300

    configure routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
-Rails.application.routes.draw do
-  
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root'users#index'
+# frozen_string_literal: true
 
-  resources :users, only: [:index, :show, :new, :create] do
-    resources :posts, only: [:index, :new, :show, :create] do
-      resources :comments, only: [:new, :create]
+Rails.application.routes.draw do
+  devise_for :users 
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :new, :show, :create, :destroy] do
+  end
+
+  resources :posts do
+    resources :comments, only: [:new, :create, :destroy]
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    resources :posts, only: [:index] do
+      resources :comments, only: [:create, :index]
     end
   end
 end
